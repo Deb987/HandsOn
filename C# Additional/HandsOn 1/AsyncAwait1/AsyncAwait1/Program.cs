a41772e1fe414b13b7fb2619cc7223095fa07120
Deb987 <79870378+Deb987@users.noreply.github.com>
2021-03-02 15:19:28 +0530

    Add files via upload

---
+++ C# Additional/HandsOn 1/AsyncAwait1/AsyncAwait1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AsyncAwait1
+{
+    class Program
+    {
+        public async static Task FirstMethod()
+        {
+            string str = await SecondMethod();
+            Console.WriteLine(str);
+        }
+        public async static Task<string> SecondMethod()
+        {
+            Thread.Sleep(5000);
+            return "Welcome in SecondMethod";
+        }
+        static void Main(string[] args)
+        {
+            FirstMethod();
+            Console.ReadLine();
+        }
+    }
+}
