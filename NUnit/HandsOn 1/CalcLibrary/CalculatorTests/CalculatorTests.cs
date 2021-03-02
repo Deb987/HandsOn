ed303dd3510f1517fbd4750ea7e422638d7c3181
Deb987 <79870378+Deb987@users.noreply.github.com>
2021-03-02 14:48:08 +0530

    Add files via upload

---
+++ NUnit/HandsOn 1/CalcLibrary/CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CalcLibrary; 
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        Calculator add;
+        [SetUp]
+        public void SetUpMethod()
+        {
+            add = new Calculator();
+        }
+        [Test]
+        [TestCase(15,10,25)]
+        [TestCase(5,6,11)]
+        public void CalcTest(double num1, double num2, double expected)
+        {
+            double result = add.Addition(num1,num2);
+            Assert.AreEqual(expected,result);
+
+        }
+        [TearDown]
+        public void TearDownMethod()
+        {
+            
+        }
+    }
+}
